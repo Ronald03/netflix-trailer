1898ab288b387f61576961650f5cb989bc1f9620
RonaldLnxDesk <ronaldceballos15@gmail.com>
2020-09-06 21:30:57 -0500

    App is fetching data from TMDB;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row.js";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <p>Testing Netflix Trailelr App</p>
-      
-      
+      <Row title="Netflix Orginals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Documentaries" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }

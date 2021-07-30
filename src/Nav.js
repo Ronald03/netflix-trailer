45ffd8ad17bcaf0872ef625cf49a0e158d808f83
ronaldC <ronaldceballos15@gmail.com>
2021-07-29 23:19:41 -0500

    Rebilt project; remove avatar added .env

---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? handleShow(true) : handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://www.pinclipart.com/picdir/big/2-23236_transparent-netflix-logo-2018-clipart.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
